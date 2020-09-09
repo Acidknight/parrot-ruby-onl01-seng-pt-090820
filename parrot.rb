4e6b34f001b51cd8678045f5d0ac9dcf440123f8
organized-key-8464 <acidknights89@gmail.com>
2020-09-09 21:35:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Pretty bird!")
-  puts phrase
-  return phrase
+def parrot
+  phrase = "Pretty bird!"
   puts "Squawk!"
   return "Squawk!"
 end
+
+parrot "phrase"
 
